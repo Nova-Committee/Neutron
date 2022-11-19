e782cf2dc43d8cead8328cbd6da9c98754d28e44
MikhailTapio <mikhailtapio13468@163.com>
2022-11-19 17:43:07 +0800

    Make /neutron * an op-only command

--- src/main/scala/committee/nova/neutron/server/command/impl/CommandNeutron.scala
+++ src/main/scala/committee/nova/neutron/server/command/impl/CommandNeutron.scala
@@ -3,6 +3,8 @@ package committee.nova.neutron.server.command.impl
 import committee.nova.neutron.server.config.ServerConfig
 import committee.nova.neutron.server.l10n.ChatComponentServerTranslation
 import net.minecraft.command.{CommandBase, ICommandSender, WrongUsageException}
+import net.minecraft.entity.player.EntityPlayerMP
+import net.minecraft.server.MinecraftServer
 
 class CommandNeutron extends CommandBase {
   override def getCommandName: String = "neutron"
@@ -19,6 +21,13 @@ class CommandNeutron extends CommandBase {
         ServerConfig.sync()
       }
       case _ => throw new WrongUsageException("msg.neutron.cmd.wrongUsage")
+    }
+  }
+
+  override def canCommandSenderUseCommand(sender: ICommandSender): Boolean = {
+    sender match {
+      case p: EntityPlayerMP => MinecraftServer.getServer.getConfigurationManager.func_152596_g(p.getGameProfile)
+      case _ => true
     }
   }
 }
